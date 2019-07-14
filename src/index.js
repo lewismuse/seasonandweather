ff4fc1caf0d32eb342b33cf023ebd9910402b09c
lewismuse <cai.gyuan@gmail.com>
2019-07-14 14:11:19 -0500

    done

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import SeasonDisplay from './SeasonDisplay';
+import Spiner from './Spiner';
 
 
 
 class App extends React.Component {
-    constructor(props) {
-        super(props);
 
-        this.state = { lat: null,errorMessage: ""};
+
+    state = { lat: null,errorMessage: ''};
+
+    componentDidMount() {
         window.navigator.geolocation.getCurrentPosition(
             (position)=>{
                 this.setState({lat: position.coords.latitude})
@@ -17,19 +19,27 @@ class App extends React.Component {
             }
         );
     }
-    render() {
+
+    renderContent() {
         if (this.state.errorMessage && !this.state.lat) {
             return (
-            <div>
-                Error: {this.state.errorMessage}
-            </div>)
+                <div>
+                    Error: {this.state.errorMessage}
+                </div>)
         }
         if (!this.state.errorMessage && this.state.lat) {
-            return <div>
-                Latitude: {this.state.lat}
-            </div>
+            return <SeasonDisplay lat={this.state.lat}/>
         }
-        return <div>Loading!</div>
+        return <Spiner message = "Please wait"/>
+    }
+
+    render() {
+        return (
+            <div className= "border red">
+                {this.renderContent()}
+            </div>
+        )
+
 
 
     }
